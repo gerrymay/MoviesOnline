0f65a220d2eaa213aed3a8a193171a0f34b9a3bb
gerrymay <gerardo.may@gmail.com>
2019-10-18 10:58:17 -0500

    initial commit

---
+++ MoviesOnLine.Data/Repositories/MovieRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MoviesOnLine.Data.Entities;
+
+namespace MoviesOnLine.Data.Repositories
+{
+    public class MovieRepository
+    {
+
+        public List<Movie> getMovies() {
+            using (var context = new MoviesDBContext())
+            {
+                return context.Movies.ToList();
+            }
+        }
+
+        public int save(Movie movie) {
+            using (var context = new MoviesDBContext()) {
+                context.Movies.Add(movie);
+                return context.SaveChanges();
+            }
+        }
+
+    }
+}
